572c4036d841e4c7078ad25332817babc126c488
Pierre Cazenave <47040310+picazenave@users.noreply.github.com>
2024-01-22 12:13:54 +0100

    working uart

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 - https://github.com/STMicroelectronics
 - https://www.st.com/en/evaluation-tools/nucleo-f401re.html#documentation
 - https://www.st.com/resource/en/datasheet/stm32f401re.pdf
+- https://www.st.com/resource/en/user_manual/um1725-description-of-stm32f4-hal-and-lowlayer-drivers-stmicroelectronics.pdf
+
+## Réu
+- estimer la trajectoir de la bestiole
+- tof traquer la bestiole pour capturer au centre/ le plus cadré possible
+- capteur particulier de ST pour PIR
+- 
--- code/src/main.c
+++ code/src/main.c
@@ -1,47 +1,22 @@
-#if F0
-#include "stm32f0xx_hal.h"
-#elif F1
-#include "stm32f1xx_hal.h"
-#elif F2
-#include "stm32f2xx_hal.h"
-#elif F3
-#include "stm32f3xx_hal.h"
-#elif F4
-#include "stm32f4xx_hal.h"
-#elif F7
-#include "stm32f7xx_hal.h"
-#elif L0
-#include "stm32l0xx_hal.h"
-#elif L1
-#include "stm32l1xx_hal.h"
-#elif L4
-#include "stm32l4xx_hal.h"
-#else
-#error "Unsupported STM32 Family"
-#endif
+#include "main.h"
 
-#define LED_PIN                                GPIO_PIN_5
-#define LED_GPIO_PORT                          GPIOA
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOA_CLK_ENABLE()
+
+UART_HandleTypeDef uart2;
 
 int main(void)
 {
   HAL_Init();
-  
-  LED_GPIO_CLK_ENABLE();
-  
-  GPIO_InitTypeDef GPIO_InitStruct;
-  
-  GPIO_InitStruct.Pin = LED_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct); 
 
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+
+  char a[7] = "hello\r\n";
   while (1)
   {
-    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-    
+    HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+    HAL_UART_Transmit(&uart2, (uint8_t *)a, 7, 1000);
     HAL_Delay(1000);
   }
 }
@@ -57,29 +32,35 @@ void NMI_Handler(void)
 
 void HardFault_Handler(void)
 {
-  while (1) {}
+  while (1)
+  {
+  }
 }
-
 
 void MemManage_Handler(void)
 {
-  while (1) {}
+  while (1)
+  {
+  }
 }
 
 void BusFault_Handler(void)
 {
-  while (1) {}
+  while (1)
+  {
+  }
 }
 
 void UsageFault_Handler(void)
 {
-  while (1) {}
+  while (1)
+  {
+  }
 }
 
 void SVC_Handler(void)
 {
 }
-
 
 void DebugMon_Handler(void)
 {
@@ -87,4 +68,88 @@ void DebugMon_Handler(void)
 
 void PendSV_Handler(void)
 {
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+//======================
+// Drivers init
+//======================
+static void MX_USART2_UART_Init(void)
+{
+
+  __USART2_CLK_ENABLE();
+  uart2.Instance = USART2;
+  uart2.Init.BaudRate = 115200;
+  uart2.Init.WordLength = UART_WORDLENGTH_8B;
+  uart2.Init.StopBits = UART_STOPBITS_1;
+  uart2.Init.Parity = UART_PARITY_NONE;
+  uart2.Init.Mode = UART_MODE_TX_RX;
+  uart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  uart2.Init.OverSampling = UART_OVERSAMPLING_16;
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  GPIO_InitStruct.Pin = USART_TX_Pin | USART_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  if (HAL_UART_Init(&uart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED2_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
